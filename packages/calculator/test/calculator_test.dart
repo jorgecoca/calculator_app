b142ec17ab682d3f3609af1928c06cc90ef70f05
Jorge Coca <jorge.coca.github@gmail.com>
2020-08-13 10:42:45 -0500

    lesson 4: structure of a good test

--- packages/calculator/test/calculator_test.dart
+++ packages/calculator/test/calculator_test.dart
@@ -2,8 +2,30 @@ import 'package:calculator/calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
+  setUpAll(() {
+    // it will execute once before any tests run
+  });
+
+  setUp(() {
+    // it will execute before every test
+  });
+
+  tearDown(() {
+    // it will execute after every test
+  });
+
+  tearDownAll(() {
+    // it will execute once after all the tests run
+  });
+
   test('the calculator returns 4 when adding 2 and 2', () {
+    // 1st step: setup -> create the calculator object
     final calculator = Calculator();
-    expect(calculator.add(2, 2), 4);
+
+    // 2nd step: side effects -> collect the result you want to test
+    final result = calculator.add(2, 2);
+
+    // 3rd step: expectations -> compare the result against an expected value
+    expect(result, 4);
   });
 }
