90264cadf050486879a6beb1ee617ef96c73c9e0
Jorge Coca <jorge.coca.github@gmail.com>
2020-08-13 15:19:51 -0500

    lesson 5: using expect() in your tests

--- packages/calculator/test/calculator_test.dart
+++ packages/calculator/test/calculator_test.dart
@@ -2,30 +2,9 @@ import 'package:calculator/calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  setUpAll(() {
-    // it will execute once before any tests run
-  });
-
-  setUp(() {
-    // it will execute before every test
-  });
-
-  tearDown(() {
-    // it will execute after every test
-  });
-
-  tearDownAll(() {
-    // it will execute once after all the tests run
-  });
-
   test('the calculator returns 4 when adding 2 and 2', () {
-    // 1st step: setup -> create the calculator object
     final calculator = Calculator();
-
-    // 2nd step: side effects -> collect the result you want to test
     final result = calculator.add(2, 2);
-
-    // 3rd step: expectations -> compare the result against an expected value
-    expect(result, 4);
+    expect(result, 4.00000, reason: 'It should be exactly 4');
   });
 }
