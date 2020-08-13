794d97d28e82777100610e488c02de406aa2c60c
Jorge Coca <jorge.coca.github@gmail.com>
2020-08-13 16:33:19 -0500

    lesson 6: all you need to know about Matchers

--- packages/calculator/test/calculator_test.dart
+++ packages/calculator/test/calculator_test.dart
@@ -2,6 +2,16 @@ import 'package:calculator/calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
+  test('Calculator creates a non null object', () {
+    expect(Calculator(), isNotNull);
+  });
+
+  test('the calculator returns an double number when adding 2 and 2', () {
+    final calculator = Calculator();
+    final result = calculator.add(2, 2);
+    expect(result, isA<double>());
+  });
+
   test('the calculator returns 4 when adding 2 and 2', () {
     final calculator = Calculator();
     final result = calculator.add(2, 2);
