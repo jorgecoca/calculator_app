c169be6d913ee836cda891aa65216b1e4e59ec99
Jorge Coca <jorge.coca.github@gmail.com>
2020-09-13 13:28:59 -0500

    lesson 19: sync stubbing with mockito

--- test/two_digit_operation_test.dart
+++ test/two_digit_operation_test.dart
@@ -16,7 +16,9 @@ void main() {
 
   group('TwoDigitOperation', () {
     group('Operation.add', () {
-      testWidgets('paints 4.0 when adding 3 and 1', (tester) async {
+      testWidgets('paints the value returned by the calculator',
+          (tester) async {
+        when(calculator.add(3, 1)).thenReturn(25);
         await tester.pumpWidget(
           MaterialApp(
             home: Scaffold(
@@ -36,7 +38,34 @@ void main() {
           '1',
         );
         await tester.pumpAndSettle();
-        expect(find.text('is 4.0'), findsOneWidget);
+        expect(find.text('is 25.0'), findsOneWidget);
+      });
+    });
+
+    group('Operation.divide', () {
+      testWidgets('paints the value returned by the calculator',
+          (tester) async {
+        when(calculator.divide(3, 0)).thenThrow(Exception('oops'));
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: TwoDigitOperation(
+                operation: Operation.divide,
+                calculator: calculator,
+              ),
+            ),
+          ),
+        );
+        await tester.enterText(
+          find.byKey(const Key('textField_top_divided by')),
+          '3',
+        );
+        await tester.enterText(
+          find.byKey(const Key('textField_bottom_divided by')),
+          '0',
+        );
+        await tester.pumpAndSettle();
+        expect(find.text('is ???'), findsOneWidget);
       });
     });
   });
