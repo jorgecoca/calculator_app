9e64f68db07a973d4d2831f1cacfa8ed994d3afa
Jorge Coca <jorge.coca.github@gmail.com>
2020-09-13 09:06:44 -0500

    lesson 16: matchers

--- test/calculator_app_test.dart
+++ test/calculator_app_test.dart
@@ -1,5 +1,6 @@
 import 'package:calculator_app/main.dart';
 import 'package:calculator_app/two_digit_operation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -9,6 +10,11 @@ void main() {
         (WidgetTester tester) async {
       await tester.pumpWidget(CalculatorApp());
       expect(find.byType(TwoDigitOperation), findsNWidgets(4));
+    });
+
+    testWidgets('there are zero buttons in the screen', (tester) async {
+      await tester.pumpWidget(CalculatorApp());
+      expect(find.byType(RaisedButton), findsNothing);
     });
 
     group('add', () {
