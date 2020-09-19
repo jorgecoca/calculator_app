cdc5115f5a57f90e306a2d45861f00001a3c9a39
Jorge Coca <jorge.coca.github@gmail.com>
2020-09-19 15:01:30 -0500

    lesson 25: take screenshots

--- test_driver/calculator_app_test.dart
+++ test_driver/calculator_app_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
@@ -22,10 +24,27 @@ void main() {
   });
 
   test('multiplying 5 and 10 shows 50', () async {
+    await driver.takeScreenshot('no_values_entered');
     await driver.tap(find.byValueKey('textField_top_multiplied by'));
     await driver.enterText('5');
+    await driver.takeScreenshot('entered_5');
     await driver.tap(find.byValueKey('textField_bottom_multiplied by'));
     await driver.enterText('10');
+    await driver.takeScreenshot('entered_10');
     await driver.waitFor(find.text('is 50.0'));
+    await driver.takeScreenshot('result_is_50');
   });
+}
+
+extension on FlutterDriver {
+  Future<void> takeScreenshot(String name) async {
+    final filePath = File('screenshots/$name.png');
+    if (await filePath.exists()) {
+      await filePath.delete(recursive: true);
+    }
+    final file = await filePath.create(recursive: true);
+    final png = await screenshot();
+    file.writeAsBytesSync(png);
+    print('screenshot with name $name was taken');
+  }
 }
